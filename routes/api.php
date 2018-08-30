66cd303ba7353bc96eaef2b9fc161e0cce4d5304
NatasaRadovanovic <natasar.radovanovic@gmail.com>
2018-08-30 20:06:48 +0200

    auth

---
+++ routes/api.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group([ //jwt
+    
+        'middleware' => 'api',
+        'prefix' => 'auth'
+    
+    ], function ($router) {
+    
+        Route::post('login', 'AuthController@login');
+        Route::post('logout', 'AuthController@logout');
+        Route::post('refresh', 'AuthController@refresh');
+        Route::post('me', 'AuthController@me');
+    
+    });
+
+Route::resource('contacts', 'ContactsController')->middleware('auth:api'); //middl se dodaje
+//za jwt
+ 
+//on je registrovao sve rute
+//na php artisan:list mozemo da vidimo koje su to rute
